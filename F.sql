4e2f29d0c2a238be5c9d157e2e0b858c2f6827eb
thaojenny <106482927+thaojenny@users.noreply.github.com>
2024-03-07 14:25:57 +0700

    Update F.sql

--- F.sql
+++ F.sql
@@ -330,7 +330,136 @@ left join [practice].[dbo].[Mã ngành VISIC] [v]
 on g.[manganhc4] = v.[Mã ngành C4 VISIC]
 where v.[Cấp 2] like '%FMCG%'
 
+---------------------------------------------------FMCG_F&B-------------------------------------------------------------------------
+	   ----------------complete table of 2021 FMCG company data-------------------
+  with FB as (
+  select '2021' as [Năm],
+		[Masothue] [Mã số thuế],
+		[TenDN] [ Tên DN],
+		[Mã ngành C5],
+		[Co_XNK],
+		
+		[DN1].[LHDN] [Loại hình DN],-----------from 2019 to 2020 look for value LHDN in table_DN1, otherwise, LHDN2
+		[TINH].[Tên] [Tỉnh],
+		[Cấp 1],
+		[Cấp 2],
+		[Cấp 3],
+		[Doanhthu_Thuan_BH_CCDV] [DTT],
+		[SR_Loinhuan_SauThue] [LNST],
+		(isnull(SR_Tien,0) + isnull(SR_DautuTC_NH,0) + isnull(SR_PhaiThu_NH,0))*1e6 [TSLD_khong_gom_HTK],
+		(isnull(SR_Phaitra_NBan_NH,0) + isnull([SR_NMua_Tratruoc_NH],0)  + isnull([SR_Thue],0) + isnull([SR_PhaiTra_NguoiLD],0) + isnull([SR_VayNoTC_NH],0) + isnull([SR_DP_Phaitra_NH],0))*1e6 [No ngan han],
+		[SR_Von_ChuSohuu_CK],
+		[SR_Nguonvon_Tong_CK],
+		[SR_Tien],
+		[SR_DautuTC_NH],
+		[SR_NoPhaitra_CK]/nullif([SR_Von_ChuSohuu_CK],0) [He so no],
+		(isnull(SR_Tien,0) + isnull(SR_DautuTC_NH,0) + isnull(SR_PhaiThu_NH,0))*1e6/nullif((isnull(SR_Phaitra_NBan_NH,0) + isnull([SR_NMua_Tratruoc_NH],0)  + isnull([SR_Thue],0) + isnull([SR_PhaiTra_NguoiLD],0) + isnull([SR_VayNoTC_NH],0) + isnull([SR_DP_Phaitra_NH],0))*1e6,0) [HSTTN],
+		[SR_Von_ChuSohuu_CK]/nullif([SR_Nguonvon_Tong_CK],0) [HSTTT],
+		[SR_Taisan_NH_CK]/nullif((isnull(SR_Phaitra_NBan_NH,0) + isnull([SR_NMua_Tratruoc_NH],0)  + isnull([SR_Thue],0) + isnull([SR_PhaiTra_NguoiLD],0) + isnull([SR_VayNoTC_NH],0) + isnull([SR_DP_Phaitra_NH],0))*1e6,0) [HSTTNHN],
+		[SR_Loinhuan_SauThue] *2 / nullif([SR_Nguonvon_Tong_CK]+[SR_Nguonvon_Tong_DK],0) [ROA],
+		[SR_Loinhuan_SauThue] *2/ nullif([SR_Von_ChuSohuu_CK]+[SR_Von_ChuSohuu_DK],0) [ROE],
+		[SR_TSCD_CK],
+		[SR_LoinhuanGop] /nullif([SR_Doanhthu_Thuan_BH_CCDV],0) [TSLN_gop],
+		365*[SR_Phaitra_NBan_NH]/nullif([SR_Gia_VonHangban]+[SR_Tonkho_CK]-[SR_Tonkho_DK],0) [VQKPTRA],
+		365/2*([SR_Gia_VonHangban]+[SR_Tonkho_CK]-[SR_Tonkho_DK])/nullif([SR_Gia_VonHangban],0) [VQHTK],
+		365*[SR_PhaiThu_NH]/nullif([SR_Doanhthu_Thuan_BH_CCDV],0) [VQKPTHU]
+from [practice].[dbo].[GSO 2021] [gso]
+left join [practice].[dbo].[Mã ngành VISIC] [mn]
+on gso.[Mã ngành C5] = mn.[Mã ngành C5 VISIC]
+left join [10.1.123.130].[MAPPING].[dbo].[LoaihinhDN_2] [DN1]
+ON gso.[LoaihinhDN] =dn1.[MA_LHDN]
+left join [10.1.123.130].[MAPPING].[dbo].[GSO_ma_tinh] [TINH]
+ON gso.[DNTB_MaTinh] = [TINH].[Mã]
+where [Cấp 2] like '%FMCG%'
 
+-----------------------------năm 2020------------------------
+union all
+
+select '2020' as [Năm],
+		[masothue] [Mã số thuế],
+		[tenDN] [Tên DN],
+		[Mã ngành C5],
+		[Co_xnk],
+		d.[LHDN],
+		t.[Tên],
+		v.[Cấp 1],
+		v.[Cấp 2],
+		v.[Cấp 3],
+		[BCTC_Doanhthu_Thuan_BH_CCDV] [DTT],
+		[SR_Loinhuan_SauThue] [LNST],
+		(isnull(SR_Tien,0) + isnull(SR_DautuTC_NH,0) + isnull(SR_PhaiThu_NH,0))*1e6 [TSLD_khong_gom_HTK],
+		(isnull(SR_Phaitra_NBan_NH,0) + isnull([SR_NMua_Tratruoc_NH],0)  + isnull([SR_Thue],0) + isnull([SR_PhaiTra_NguoiLD],0) + isnull([SR_VayNoTC_NH],0) + isnull([SR_DP_Phaitra_NH],0))*1e6 [No ngan han],
+		[SR_Von_ChuSohuu_CK],
+		[SR_Nguonvon_Tong_CK],
+		[SR_Tien],
+		[SR_DautuTC_NH],
+		[SR_NoPhaitra_CK]/nullif([SR_Von_ChuSohuu_CK],0) [He so no],
+		(isnull(SR_Tien,0) + isnull(SR_DautuTC_NH,0) + isnull(SR_PhaiThu_NH,0))*1e6/nullif((isnull(SR_Phaitra_NBan_NH,0) + isnull([SR_NMua_Tratruoc_NH],0)  + isnull([SR_Thue],0) + isnull([SR_PhaiTra_NguoiLD],0) + isnull([SR_VayNoTC_NH],0) + isnull([SR_DP_Phaitra_NH],0))*1e6,0) [HSTTN],
+		[SR_Von_ChuSohuu_CK]/nullif([SR_Nguonvon_Tong_CK],0) [HSTTT],
+		[SR_Taisan_NH_CK]/nullif((isnull(SR_Phaitra_NBan_NH,0) + isnull([SR_NMua_Tratruoc_NH],0)  + isnull([SR_Thue],0) + isnull([SR_PhaiTra_NguoiLD],0) + isnull([SR_VayNoTC_NH],0) + isnull([SR_DP_Phaitra_NH],0))*1e6,0) [HSTTNHN],
+		[SR_Loinhuan_SauThue] *2 / nullif([SR_Nguonvon_Tong_CK]+[SR_Nguonvon_Tong_DK],0) [ROA],
+		[SR_Loinhuan_SauThue] *2/ nullif([SR_Von_ChuSohuu_CK]+[SR_Von_ChuSohuu_DK],0) [ROE],
+		[SR_TSCD_CK],
+		[SR_LoinhuanGop] /nullif([SR_Doanhthu_Thuan_BH_CCDV],0) [TSLN_gop],
+		365*[SR_Phaitra_NBan_NH]/nullif([SR_Gia_VonHangban]+[SR_Tonkho_CK]-[SR_Tonkho_DK],0) [VQKPTRA],
+		365/2*([SR_Gia_VonHangban]+[SR_Tonkho_CK]-[SR_Tonkho_DK])/nullif([SR_Gia_VonHangban],0) [VQHTK],
+		365*[SR_PhaiThu_NH]/nullif([SR_Doanhthu_Thuan_BH_CCDV],0) [VQKPTHU]
+from GSO_2020 [g]
+left join [10.1.123.130].[MAPPING].[dbo].[LoaihinhDN] [d]
+on g.[loaihinhkt] = d.[MA_LHDN]
+left join [10.1.123.130].[MAPPING].[dbo].[GSO_ma_tinh] [t]
+ON g.[DNTB_MaTinh] = t.[Mã]
+left join [practice].[dbo].[Mã ngành VISIC] [v]
+on g.[Mã ngành C5] = v.[Mã ngành C5 VISIC]
+where v.[Cấp 2] like '%FMCG%'
+		
+union all
+-----------------------------năm 2019------------------------
+
+select '2019' as [Năm],
+		[masothue] [Mã số thuế],
+		[tenDN] [Tên DN],
+		[manganhc4] [Mã ngành C4],
+		[Co_XNK],
+		d.[LHDN],
+		t.[Tên],
+		v.[Cấp 1],
+		v.[Cấp 2],
+		v.[Cấp 3],
+		[BCTC_Doanhthu_Thuan_BH_CCDV] [DTT],
+		[SR_Loinhuan_SauThue] [LNST],
+		(isnull(SR_Tien,0) + isnull(SR_DautuTC_NH,0) + isnull(SR_PhaiThu_NH,0))*1e6 [TSLD_khong_gom_HTK],
+		(isnull(SR_Phaitra_NBan_NH,0) + isnull([SR_NMua_Tratruoc_NH],0)  + isnull([SR_Thue],0) + isnull([SR_PhaiTra_NguoiLD],0) + isnull([SR_VayNoTC_NH],0) + isnull([SR_DP_Phaitra_NH],0))*1e6 [No ngan han],
+		[SR_Von_ChuSohuu_CK],
+		[SR_Nguonvon_Tong_CK],
+		[SR_Tien],
+		[SR_DautuTC_NH],
+		[SR_NoPhaitra_CK]/nullif([SR_Von_ChuSohuu_CK],0) [He so no],
+		(isnull(SR_Tien,0) + isnull(SR_DautuTC_NH,0) + isnull(SR_PhaiThu_NH,0))*1e6/nullif((isnull(SR_Phaitra_NBan_NH,0) + isnull([SR_NMua_Tratruoc_NH],0)  + isnull([SR_Thue],0) + isnull([SR_PhaiTra_NguoiLD],0) + isnull([SR_VayNoTC_NH],0) + isnull([SR_DP_Phaitra_NH],0))*1e6,0) [HSTTN],
+		[SR_Von_ChuSohuu_CK]/nullif([SR_Nguonvon_Tong_CK],0) [HSTTT],
+		[SR_Taisan_NH_CK]/nullif((isnull(SR_Phaitra_NBan_NH,0) + isnull([SR_NMua_Tratruoc_NH],0)  + isnull([SR_Thue],0) + isnull([SR_PhaiTra_NguoiLD],0) + isnull([SR_VayNoTC_NH],0) + isnull([SR_DP_Phaitra_NH],0))*1e6,0) [HSTTNHN],
+		[SR_Loinhuan_SauThue] *2 / nullif([SR_Nguonvon_Tong_CK]+[SR_Nguonvon_Tong_DK],0) [ROA],
+		[SR_Loinhuan_SauThue] *2/ nullif([SR_Von_ChuSohuu_CK]+[SR_Von_ChuSohuu_DK],0) [ROE],
+		[SR_TSCD_CK],
+		[SR_LoinhuanGop] /nullif([SR_Doanhthu_Thuan_BH_CCDV],0) [TSLN_gop],
+		365*[SR_Phaitra_NBan_NH]/nullif([SR_Gia_VonHangban]+[SR_Tonkho_CK]-[SR_Tonkho_DK],0) [VQKPTRA],
+		365/2*([SR_Gia_VonHangban]+[SR_Tonkho_CK]-[SR_Tonkho_DK])/nullif([SR_Gia_VonHangban],0) [VQHTK],
+		365*[SR_PhaiThu_NH]/nullif([SR_Doanhthu_Thuan_BH_CCDV],0) [VQKPTHU]
+from [10.1.123.130].[CLKD].[dbo].[DATA_GSO_2020] [g]
+left join [10.1.123.130].[MAPPING].[dbo].[LoaihinhDN] [d]
+on g.[loaihinhkt] = d.[MA_LHDN]
+left join [10.1.123.130].[MAPPING].[dbo].[GSO_ma_tinh] [t]
+ON g.[DNTB_MaTinh] = t.[Mã]
+left join [practice].[dbo].[Mã ngành VISIC] [v]
+on g.[manganhc4] = v.[Mã ngành C4 VISIC]
+where v.[Cấp 2] like '%FMCG%'
+)
+select [Năm], [Mã số thuế], [ Tên DN], FB.[Mã ngành C5], [Co_XNK], [Loại hình DN],f1.[Tiểu ngành FMCG], f1.[Nhóm], [Tỉnh], [Cấp 1],[Cấp 2],[Cấp 3],DTT, LNST, TSLD_khong_gom_HTK,[No ngan han], SR_Von_ChuSohuu_CK, SR_Nguonvon_Tong_CK, SR_Tien, SR_DautuTC_NH, [He so no], HSTTN, HSTTT, HSTTNHN, ROA, ROE, SR_TSCD_CK, TSLN_gop,VQKPTRA, VQHTK, VQKPTHU
+from FB 
+left join [practice].[dbo].[F&B_map] [f1]
+on FB.[Mã ngành C5] = f1.[Mã ngành C5]
+where f1.[Tiểu ngành FMCG] is not null
+	  
 	  
 ----------------------------------------------------NHS-CASA----------------------------------------------------------------------------
 ---------------CASA BQ THÁNG NĂM 2023 (TỪ 50K ĐẾN DƯỚI 10TR) KHCN
